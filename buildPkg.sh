216aef619553dcf421f8354736f23a31d827d132
andreas <andreas@wellenvogel.de>
2023-04-16 20:59:32 +0200

    inital run

---
+++ buildPkg.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+#package build using https://github.com/goreleaser/nfpm on docker
+err(){
+  echo "ERROR: $*"
+  exit 1
+}
+pdir=`dirname $0`
+pdir=`readlink -f "$pdir"`
+#set -x
+for sub in build src
+do
+
+  if [ -d "$pdir/$sub" ] ; then
+    rm -rf "$pdir/$sub"
+  fi  
+  mkdir -p "$pdir/$sub" || err "unable to create $pdir/$sub"
+done
+
+for dir in RTL8188EU
+do
+  dn="$pdir/$dir"
+  [ ! -f "$dn/module" ] && err "$dn/module missing"
+  echo "collecting $dir"
+  mv=`cat "$dn/module"`
+  echo "$dir=$mv" >> "$pdir/build/allmodules" 
+  "$dn/prepare.sh" || err "unable to run $dn/prepare.sh"
+done
+config=package.yaml
+version="$1"
+if [ "$version" = "" ] ; then
+  version=`date '+%Y%m%d'`
+fi
+echo building version $version
+tmpf=package$$.yaml
+rm -f $tmpf
+sed "s/^ *version:.*/version: \"$version\"/" $config > $tmpf
+config=$tmpf
+docker run  --rm   -v "$pdir":/tmp/pkg   --user `id -u`:`id -g` -w /tmp/pkg goreleaser/nfpm:v2.28.0 pkg -p deb -f $config
+rt=$?
+if [ "$tmpf" != "" ] ; then
+  rm -f $tmpf
+fi
+exit $rt
