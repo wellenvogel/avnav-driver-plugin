dbf8cccff2f010aaf71db0587171cd397f74e5c6
andreas <andreas@wellenvogel.de>
2023-04-16 20:08:23 +0200

    1st prepare

--- RTL8188EU/prepare.sh
+++ RTL8188EU/prepare.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
+err(){
+    echo "ERROR: $*"
+    exit 1
+}
 DLNAME=RTL8188EU.zip
 pdir="`dirname $0`"
 pdir="`readlink -f \"$pdir\"`"
 BUILD="$pdir/../build"
 SRC="$pdir/../src"
-TNAME=`cat "$PDIR/module" | tr / -`
+MODULE=`awk 'BEGIN {FS="/"};{print $1;}' < "$pdir/module"`
+VERSION=`awk 'BEGIN {FS="/"};{print $2;}' < "$pdir/module"`
+TNAME="$MODULE-$VERSION"
 rm -f "$BUILD/$DLNAME"
-( cd $BUILD && curl -o $DLNAME https://github.com/lwfinger/rtl8188eu/archive/refs/heads/v5.2.2.4.zip ) || exit 1
+rm -rf "$SRC/$TNAME"
+( cd $BUILD && curl -L  -o $DLNAME "https://github.com/lwfinger/rtl8188eu/archive/refs/heads/v5.2.2.4.zip" ) || exit 1
 ( cd $SRC &&  unzip "$BUILD/$DLNAME")
-for f in patch/*
+[ ! -d "$SRC/$TNAME" ] && err "$SRC/$TNAME not found after unpack"
+for f in $pdir/patch/*
 do
     bn=`basename $f`
-    cp $f "$SRC/$bn" || exit 1
-    sed -i -e "s/#MODULE#/$TNAME" "$SRC/$bn"
+    cp $f "$SRC/$TNAME/$bn" || exit 1
+    sed -i -e "s/#MODULE#/$MODULE/" -e "s/#VERSION#/$VERSION/" "$SRC/$TNAME/$bn"
 done      
